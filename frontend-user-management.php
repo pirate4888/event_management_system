42821a02332b2942cf2f5305ee45fcabbdaf982a
SchwarzwaldFalke_kathib <admin@schwarzwald-falke.de>
2014-01-18 18:06:07 +0100

    Experimental branch test

--- frontend-user-management.php
+++ frontend-user-management.php
@@ -3,7 +3,7 @@
  * Plugin Name: Frontend User Management
  * Plugin URI: https://github.com/SchwarzwaldFalke/frontend-user-management
  * Description: Plugin which allows user to register, login and edit their user profile in frontend. It also adds activation mails during user registration
- * Version: 0.05
+ * Version: 0.06
  * Author: Christoph Bessei
  * Author URI: https://www.schwarzwald-falke.de
  * License: GPL v2

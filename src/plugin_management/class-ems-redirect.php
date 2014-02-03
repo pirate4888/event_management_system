0c62a10cd81932a960956474771474e61c9b0579
Christoph Bessei <admin@schwarzwald-falke.de>
2014-02-03 01:46:49 +0100

    event_management_system-6 event registration page throws 404 error
    renamed GET Parameter 'event' to 'ems_event' because 'event' seems to be reserved by wordpress
    added redirection for 'event' to avoid broken links
    
    fixes #6

--- src/plugin_management/class-ems-redirect.php
+++ src/plugin_management/class-ems-redirect.php
@@ -5,10 +5,12 @@
  * @version
  */
 class Ems_Redirect {
+
 	/**
 	 * Redirects fum_event_registration with 'event' parameter to fum_event_registration with Fum_Event_Registration_Controller::$event_get_parameter
 	 * This necessary because 'event' seems to be a reserved parameter in wordpress
 	 */
+
 	public static function redirect_event_parameter() {
 		global $wp;
 		if ( isset( $_REQUEST['event'] ) && url_to_postid( trim( site_url( $wp->request ), '/' ) ) == get_option( Fum_Conf::$fum_event_registration_page ) ) {

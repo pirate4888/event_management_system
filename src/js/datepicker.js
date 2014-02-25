99483e9c97c3dccf69e3a79eaa9169a6aa89d211
Christoph Bessei <admin@schwarzwald-falke.de>
2014-02-26 00:13:59 +0100

    Initial import - add internationalized datepicker and datepicker period scripts

---
+++ src/js/datepicker.js
@@ -0,0 +1,20 @@
+/**
+ * @author Christoph Bessei
+ * @version 0.04
+ */
+jQuery(document).ready(function () {
+	jQuery('.ems_datepicker').datepicker({
+		// Show the 'close' and 'today' buttons
+		showButtonPanel: true,
+		closeText      : objectL10n.closeText,
+		currentText    : objectL10n.currentText,
+		monthNames     : objectL10n.monthNames,
+		monthNamesShort: objectL10n.monthNamesShort,
+		dayNames       : objectL10n.dayNames,
+		dayNamesShort  : objectL10n.dayNamesShort,
+		dayNamesMin    : objectL10n.dayNamesMin,
+		dateFormat     : objectL10n.dateFormat,
+		firstDay       : objectL10n.firstDay,
+		isRTL          : objectL10n.isRTL
+	});
+});

87c0fbcb22ef32ab061392b5633b29397064272b
Christoph Bessei <admin@schwarzwald-falke.de>
2014-02-26 16:47:01 +0100

    Fixed "ghost posts" after deactivation of plugin, 'posts_per_page' is by default 5 so before only 5 fum_posts where deleted. -1 means "all" and fix this issue

--- lib/frontend-user-management/src/model/class-fum-post.php
+++ lib/frontend-user-management/src/model/class-fum-post.php
@@ -1,20 +1,20 @@
 <?php
+
 /**
  * @author  Christoph Bessei
  * @version 0.01
  */
-
 class Fum_Post {
 
 	public static function fum_register_post_type() {
 		if ( ! post_type_exists( Fum_Conf::$fum_post_type ) ) {
 			$args = array(
-				'public'              => true,
-				'label'               => Fum_Conf::$fum_post_type_label,
-				'exclude_from_search' => true,
-				'show_ui'             => false,
-				'show_in_nav_menus'   => false,
-				'rewrite'             => array( 'slug' => '', 'with_front' => false ),
+					'public'              => true,
+					'label'               => Fum_Conf::$fum_post_type_label,
+					'exclude_from_search' => true,
+					'show_ui'             => false,
+					'show_in_nav_menus'   => false,
+					'rewrite'             => array( 'slug' => '', 'with_front' => false ),
 			);
 			register_post_type( Fum_Conf::$fum_post_type, $args );
 		}
@@ -22,14 +22,14 @@ class Fum_Post {
 
 	public static function add_post( $name, $title, $content ) {
 		$fum_post = array(
-			'comment_status' => 'closed',
-			'ping_status'    => 'closed',
-			'post_author'    => get_current_user_id(),
-			'post_status'    => 'publish',
-			'post_type'      => Fum_Conf::$fum_post_type,
-			'post_content'   => $content,
-			'post_name'      => $name,
-			'post_title'     => $title,
+				'comment_status' => 'closed',
+				'ping_status'    => 'closed',
+				'post_author'    => get_current_user_id(),
+				'post_status'    => 'publish',
+				'post_type'      => Fum_Conf::$fum_post_type,
+				'post_content'   => $content,
+				'post_name'      => $name,
+				'post_title'     => $title,
 		);
 
 		$post_id = wp_insert_post( $fum_post );
@@ -41,7 +41,7 @@ class Fum_Post {
 	}
 
 	public static function remove_all_fum_posts() {
-		$posts = get_posts( array( 'post_type' => Fum_Conf::$fum_post_type ) );
+		$posts = get_posts( array( 'post_type' => Fum_Conf::$fum_post_type, 'posts_per_page' => - 1, ) );
 		foreach ( $posts as $post ) {
 			wp_delete_post( $post->ID );
 		}

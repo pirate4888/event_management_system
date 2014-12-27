547be77870243224d29cce8e4f02f2bd3a87dd1c
Christoph Bessei <admin@schwarzwald-falke.de>
2014-12-27 19:26:49 +0100

    Add Event Statistic

--- src/controller/class-ems-event-statistic-controller.php
+++ src/controller/class-ems-event-statistic-controller.php
@@ -9,11 +9,26 @@ class Ems_Event_Statistic_Controller {
 		$events = Ems_Event::get_events();
 		if ( ! empty( $events ) ) {
 			$startdate_oldest_event = null;
-			for ( $i = 0; ! $startdate_oldest_event instanceof DateTime && count( $events ) > $i; $i ++ ) {
-				$startdate_oldest_event = $events[0]->get_start_date_time();
+			$startdate_latest_event = null;
+			//TODO Maybe faster if we first start at the beginning of the array and search the oldest event, then start again the end and search the latest event
+			for ( $i = 0; $i < count( $events ); $i ++ ) {
+				$start_date_time = $events[ $i ]->get_start_date_time();
+				if ( ! $start_date_time instanceof DateTime ) {
+					continue;
+				}
+				/** $start_date_time DateTime */
+				//Set first event with start date as "oldest event"
+				if ( null == $startdate_oldest_event ) {
+					$startdate_oldest_event = $events[0]->get_start_date_time();
+				}
+				//Set last event with start date as "latest event"
+				if ( null === $startdate_latest_event || $start_date_time->getTimestamp() > $startdate_latest_event->getTimestamp() ) {
+					/** $startdate_latest_event DateTime */
+					$startdate_latest_event = $start_date_time;
+				}
 			}
 
-			if ( $startdate_oldest_event instanceof DateTime ) {
+			if ( null !== $startdate_oldest_event && null !== $startdate_latest_event ) {
 				$year = date( "Y", $startdate_oldest_event->getTimestamp() );
 				for ( $current_year = date( "Y" ); $year <= $current_year; $year ++ ) {
 					echo "<h3>" . $year . "</h3>";
@@ -23,6 +38,7 @@ class Ems_Event_Statistic_Controller {
 				}
 			}
 
+			return true;
 		}
 		echo "Couldn't create statistic. No Events found.";
 	}

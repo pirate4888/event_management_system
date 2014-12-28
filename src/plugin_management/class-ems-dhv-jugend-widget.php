452dbdb498d78e0c0a072ecd91cfd4cc79590488
Christoph Bessei <admin@schwarzwald-falke.de>
2014-12-28 21:48:02 +0100

    Add Date to Menu

--- src/plugin_management/class-ems-dhv-jugend-widget.php
+++ src/plugin_management/class-ems-dhv-jugend-widget.php
@@ -39,7 +39,7 @@ class Ems_Dhv_Jugend_Widget extends WP_Widget {
 					<a href="<?php echo get_permalink( get_option( Ems_Conf::EMS_NAME_PREFIX . 'partcipant_list_page' ) ); ?>">Teilnehmerlisten</a>
 				</li>
 			<?php endif; ?>
-			<?php if ( is_user_logged_in() ): ?>
+			<?php if ( is_user_logged_in() && ( current_user_can( 'read_event' ) || current_user_can( "read_" . Ems_Conf::EMS_NAME_PREFIX . "event" ) ) ): ?>
 				<li>
 					<a href="<?php echo get_permalink( get_option( Ems_Conf::EMS_NAME_PREFIX . 'event_statistic_page' ) ); ?>">Event Statistiken</a>
 				</li>
